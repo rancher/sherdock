52f9240405ac0ffa69ef2d42bdae26131d6bffb3
Vincent Fiduccia <vincent@rancher.com>
2015-06-21 04:10:54 -0700

    more detail

--- sherdock-ember/app/images/route.js
+++ sherdock-ember/app/images/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.get('store').rawRequest({url: 'images?detailed=false'});
+    return this.get('store').rawRequest({url: 'images?detailed=false'})
   }
 });
