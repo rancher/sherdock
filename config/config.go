8812e518b0371fcb85097fe19fb6bd8d3ed7156e
James Harris <james@rancher.com>
2015-06-20 23:44:01 -0700

    This deserves a commit

--- config/config.go
+++ config/config.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 )
 type Config struct {
-	pullIntervalMinutes int
-	images []string
+	PullIntervalMinutes int
+	Images []string
 }
 
 func configFileName(name string) string{
@@ -17,21 +17,22 @@ func configFileName(name string) string{
 	return name
 }
 
-func defaultConfig() *Config{
+func defaultConfig() *Config {
 	config := Config{}
-	config.images = []string {}
-	config.pullIntervalMinutes = 60
+	config.Images = []string { "JAMESCONFIG" }
+	config.PullIntervalMinutes = 60
 	return &config
 }
 
 func GetConfig(name string) (*Config, error) {
-	a := Config{}
+	config := Config{}
 	b := true
 	if name == "" {
 		b = false
 	}
 	data, err := ioutil.ReadFile(configFileName(name))
 	if err != nil && b {
+		fmt.Println( "Failing" )
 		return  nil, err
 	}
 	if err != nil && !b {
@@ -39,26 +40,35 @@ func GetConfig(name string) (*Config, error) {
 		SaveConfig(a, "")
 		return a, nil
 	}
-	err = yaml.Unmarshal(data, &a)
-	return &a , err
+	fmt.Printf( "%s", data )
+	err = yaml.Unmarshal(data, &config)
+	fmt.Printf( "%#v After unmarshal \n", config)
+	if  err == nil {
+		fmt.Printf( "%#v No Err\n", config)
+		return &config, nil
+	} else  {
+		fmt.Printf( "%#v Err\n", config)
+		fmt.Printf( "%v Err\n", err)
+		return nil, err
+	}
 }
 
 func SaveConfig(config *Config, name string) error {
 	data, err := yaml.Marshal(&config)
+	if err != nil {
+		fmt.Println("Failed to marshal the config.")
+		return nil
+	}
+	fmt.Printf("%s", data)
 	ioutil.WriteFile(configFileName(name), data, 0644)
 	return err
 }
 
 func main() {
 	config, err := GetConfig("")
-	if  err == nil {
-		fmt.Printf("%#v", config)
-	} else  {
-		fmt.Println(err)
-	}
-	config.images = append(config.images, "james")
-	err = SaveConfig(config, "")
-	if  err != nil {
-		fmt.Println("You failed")
+	if err == nil {
+		fmt.Printf( "Config %#v" , config)
+	} else {
+		fmt.Printf( "Err %#v", err)
 	}
 }
